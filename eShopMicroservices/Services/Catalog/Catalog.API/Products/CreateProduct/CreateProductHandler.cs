35ecc797a5a9c4992c4b6172f5fb67889fe24b2e
Ramkumar <USER@DESKTOP-72J84FU>
2024-07-04 19:45:58 -0700

    End of section 6

---
+++ eShopMicroservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    public class CreateProductCommandHandler(IDocumentSession session) 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //1.create product entity from command object
+            //2.save to db
+            //3. return CreateProductResult result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //save to db - yet to implement 
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            //return result 
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
